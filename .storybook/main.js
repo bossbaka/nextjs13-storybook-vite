ad96db5e94b026cd7a52171369c71855e50e6434
bossbaka <phisek.boss@gmail.com>
2022-10-29 20:19:25 +0700

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: ["../stories/*.stories.@(mdx|js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  core: {
+    builder: "storybook-builder-vite",
+  },
+};
